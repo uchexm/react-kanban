97f05a7a7662eec20ed48e5a063f71a7b8d98465
uchexm <109084069+uchexm@users.noreply.github.com>
2023-01-31 10:51:05 +0200

    Group tasks 1, 2, 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MyProfile from './components/myProfile/myProfile';
-import Missions from './components/missions/missions';
-import Rockets from './components/rockets/rockets';
-import Nav from './components/pages/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/myprofile" element={<MyProfile />} />
-        </Routes>
-      </Router>
+      <h2>It works</h2>
     </div>
   );
 }
