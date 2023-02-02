029c6af670ec0c6adc969c46533f86e1bd9da5f1
Becky449 <rabnab12@gmail.com>
2023-02-02 11:58:46 +0300

    Added myProfile.js and myProfile.css files

--- src/components/myProfile/myProfile.js
+++ src/components/myProfile/myProfile.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <table>
-    <tr>
-      <th>My Missions</th>
-      <th>My Rockets</th>
-    </tr>
-    <tr className="missions">
-      <td>Falcon 9</td>
-    </tr>
-    <tr className="rockets">
-      <td>Centro comercial Moctezuma</td>
-    </tr>
-  </table>
-);
+import './myProfile.css';
+
+function MyProfile() {
+  const missions = useSelector((state) => state.mission);
+  const missionFilter = missions.filter((mission) => mission.joinmission === true);
+
+  // Reserve rocket selector
+  const rockets = useSelector((state) => state.rocket);
+  const reserves = rockets.filter((rocket) => rocket.reserved === true);
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <article className="rocketProfileContainer">
+        <h1 className="title">My Mission</h1>
+        {missionFilter.length !== 0 ? (
+          <ul className="table">
+            {missionFilter.map((mission) => (
+              <li key={mission.id}>
+                {mission.mission}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>
+            <em>You don&apos;t have any missions joined at the moment</em>
+          </p>
+        )}
+      </article>
+      <article className="rocketProfileContainer">
+        <h1 className="title">My Rockets</h1>
+        {reserves.length !== 0 ? (
+          <ul className="table">
+            {reserves.map((reserve) => (
+              <li key={reserve.id}>
+                {reserve.name}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>
+            <em>You don&apos;t have any reserves at the moment</em>
+          </p>
+        )}
+      </article>
+    </div>
+  );
+}
 
 export default MyProfile;
