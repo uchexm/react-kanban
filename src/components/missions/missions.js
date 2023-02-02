6b11c8e4fe895ad766f48c36e99b43aef103e565
Becky449 <rabnab12@gmail.com>
2023-02-02 12:02:17 +0300

    Added mission data

--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Missions = () => <div>Mission Section</div>;
+import Mission from './Mission';
+import MissionHeader from './MissionHeader';
+import Footer from './footer';
+import { fetchApi } from '../../redux/missions/missions';
+
+function Missions() {
+  const mission = useSelector((state) => state.mission);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!mission.length) dispatch(fetchApi());
+  }, []);
+
+  return (
+    <div>
+      <MissionHeader />
+      {mission.map((data, index) => (
+        <Mission
+          bgClass={index % 2 === 0 ? 'backgroundGrey' : ''}
+          key={data.id}
+          id={data.id}
+          missionName={data.mission}
+          description={data.description}
+          joinmission={data.joinmission}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
 
 export default Missions;
